f83bde6247d494d88e8612f24c299a007bd1cff7
Carlos Silveira <carlos.dt.cd@gmail.com>
2022-12-30 17:16:41 -0100

    Create items card grid and pass props to each card

--- src/components/ItemCard/index.jsx
+++ src/components/ItemCard/index.jsx
@@ -1,21 +1,27 @@
 import React from 'react';
+import { useState } from 'react';
 
-const ItemCard = () => {
+const ItemCard = ( { name, description, category, price, image_url } ) => {
+  const [isHovering, setIsHovering] = useState(false);
+
   return (
-    <div className="w-4/4 px-2 py-4">
+    <div className="`w-4/4 px-2 py-4"
+    onMouseEnter={() => setIsHovering(true)}
+    onMouseLeave={() => setIsHovering(false)}
+    >
       <div className="border rounded-lg shadow-md overflow-hidden">
         <div className="relative h-48">
-          <img src="https://images.unsplash.com/photo-1672330145676-03a5b7cf7bde?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80" alt="title" className="w-full h-full object-cover" />
-          <span className="absolute top-0 right-0 text-white bg-green-400 rounded-full px-3 py-1 mt-1">$150</span>
+          <img src={ image_url } alt={ name } className="w-full h-full object-cover object-center" />
+          <span className="absolute top-0 right-0 text-white bg-green-400 rounded-full px-3 py-1 mt-1">${ price }</span>
         </div>
         <div className="p-4">
-          <small className="text-green-500 text-xs uppercase">CATEGORY</small>
-          <h3 className="font-bold text-xl mb-2 mr-20">THIS IS A TITLE</h3>
-          <p className="text-sm tracking-tight font-light text-slate-600 leading-6">Dodge is an American brand of automobiles and a division of Stellantis, based in Auburn Hills, Michigan..</p>
+          <small className="text-green-500 text-xs uppercase">{ category }</small>
+          <h3 className={`font-bold text-xl mb-2 mr-20 ${isHovering ? '' : 'truncate'}`}>{ name }</h3>
+          <p className={`text-sm tracking-tight font-light text-slate-600 leading-6 ${isHovering ? '' : 'truncate'}`}>{ description }</p>
         </div>
       </div>
     </div>
   )
 }
 
-export default ItemCard
+export default ItemCard;
--- src/components/ItemsCardsGrid/index.jsx
+++ src/components/ItemsCardsGrid/index.jsx
@@ -1,10 +1,33 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
 import ItemCard from '../ItemCard';
 
 const ItemsCardsGrid = () => {
 
+  // Set the state of the items, this will be an array with objects.
+  const[items, setItems] = useState([]);
+  //Fetch all the items from the API, everytime the component reloads.
+  useEffect(()=>{
+    // Send Get Request to recieve all the items on the data base
+    axios.get("http://localhost:3000/items")
+    // The response will be an object with several objects, we want extract the data.
+    .then(response => setItems(response.data))
+  },[]);
+
+console.log(items)
   return (
     <div className="container mx-auto flex flex-wrap">
+      {items.map(item =>(
+        <div className='w-1/4 p-2'>
+          <ItemCard key={item.id}
+            name={item.name}
+            price={item.price}
+            description={item.description}
+            category={item.category}
+            image_url={item.image_url}
+          />
+        </div>
+      ))}
       <div className='w-1/4 p-2'>
         <ItemCard/>
       </div>
