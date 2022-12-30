462cfe2374d83ca0eea76c6d20b8788867c6eb5b
Carlos Silveira <carlos.dt.cd@gmail.com>
2022-12-30 14:53:54 -0100

    Create FAQ component and import it to the landing page

--- src/pages/LandingPage/index.jsx
+++ src/pages/LandingPage/index.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Navbar from '../../components/Navbar';
 import Banner from '../../components/Banner';
 import InformationCards from '../../components/InformationCards';
+import Faq from '../../components/Faq';
 
 function LandingPage({ title, user }) {
 
@@ -10,6 +11,7 @@ function LandingPage({ title, user }) {
       <Navbar user={user}/>
       <Banner/>
       <InformationCards/>
+      <Faq />
     </>
   )
 }
