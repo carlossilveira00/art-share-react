1e520ec5db0bb6d1e9df8159c4e86a7258f37f17
Carlos Silveira <carlos.dt.cd@gmail.com>
2022-12-31 18:52:32 -0100

    Create component for Item Information

--- src/pages/ItemPage/index.jsx
+++ src/pages/ItemPage/index.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import ItemImageGallery from '../../components/ItemImageGallery';
 import Navbar from '../../components/Navbar';
+import ItemDescription from '../../components/ItemDescription';
 
 const ItemPage = ({user}) => {
   return (
   <>
     <Navbar user={user}/>
     <ItemImageGallery />
+    <ItemDescription/>
   </>
   )
 }
