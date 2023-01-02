40533f97ecbb8e4980c0d5708876077f083f2de0
Carlos Silveira <carlos.dt.cd@gmail.com>
2023-01-02 15:20:23 -0100

    Write the logic to the handleSubmit and handlChange and pass these functions to the Create Item form component

--- src/pages/CreateRentalItemPage/index.jsx
+++ src/pages/CreateRentalItemPage/index.jsx
@@ -2,24 +2,30 @@ import React, { useState } from 'react';
 import Navbar from '../../components/Navbar';
 import Footer from '../../components/Footer';
 import CreateItemForm from '../../components/CreateItemForm';
+import axios from 'axios';
 
 const CreateRentalItemPage = ({ user }) => {
   // Set Item information that will be send to the DB through a POST request to create an Item.
   const [itemInformation, setItemInformation] = useState({});
   // Handle the change of of each field in the form to then set the state of the itemInformation.
-  const handleChange = () => {
-
+  const handleChange = (event) => {
+    event.preventDefault();
+    setItemInformation(Object.assign({}, itemInformation, {[event.target.name]: event.target.value},{user_id: user.user_information.id}))
   };
   // Handle the submit will be the function that will be called when the user submits the form.
   // This function will take care of taking the itemInformation state and sending it to the API through an POST request.
   const handleSubmit = () => {
-
+    axios.post('http://localhost:3000/items', itemInformation)
+    .then(response => console.log(response))
   };
 
   return (
     <>
       <Navbar user={user}/>
-      <CreateItemForm />
+      <CreateItemForm
+        handleChange={handleChange}
+        handleSubmit={handleSubmit}
+      />
       <Footer/>
     </>
   )
