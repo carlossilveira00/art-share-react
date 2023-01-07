d4e6db989d9daf02b2d718aa2beed21f19c65882
Carlos Silveira <carlos.dt.cd@gmail.com>
2023-01-07 19:48:26 -0100

    Add margin left to the price tag in the item card.

--- src/components/ItemCard/index.jsx
+++ src/components/ItemCard/index.jsx
@@ -13,7 +13,7 @@ const ItemCard = ( { id,name, description, category, price, image_url } ) => {
       <div className="border rounded-lg shadow-md overflow-hidden">
         <div className="relative h-48">
           <img src={ image_url } alt={ name } className="w-full h-full object-cover object-center" />
-          <span className="absolute top-0 right-0 text-white bg-green-400 rounded-full px-3 py-1 mt-1">${ price }</span>
+          <span className="absolute mr-1 top-0 right-0 text-white bg-green-400 rounded-full px-3 py-1 mt-1">${ price }</span>
         </div>
         <div className="p-4">
           <small className="text-green-500 text-xs uppercase">{ category }</small>
